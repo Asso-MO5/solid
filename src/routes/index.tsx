1a46c7f7aa614f99459215eba842bd5f030c1097
Kazerlelutin <79605267+kazerlelutin@users.noreply.github.com>
2025-12-15 21:41:01 +0100

    modification des stats (#3)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,4 @@
-import { PeopleInMuseum } from "~/features/stats/people-in-museum"
 import { TicketStats } from "~/features/stats/ticket-stats"
-
 
 const HomePage = () => {
 
@@ -8,7 +6,6 @@ const HomePage = () => {
   return (
     <div class="h-full w-full  relative overflow-y-auto ">
       <div class="absolute inset-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-        <PeopleInMuseum />
         <TicketStats />
         {/** TODO: Add more stats */}
         <div />
