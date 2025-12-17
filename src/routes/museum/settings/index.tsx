0e5daf92deb5a1512db05017d2b92e596fa81d79
Kazerlelutin <b@bouteiller.contact>
2025-12-18 00:42:38 +0100

    wip

--- src/routes/museum/settings/index.tsx
+++ src/routes/museum/settings/index.tsx
@@ -11,56 +11,78 @@ const CapacityPage = () => {
   const { setting: slotSetting, setSetting: setSlotSetting, saveSetting: saveSlotSetting, description: slotDescription, setDescription: setSlotDescription } = settingNumCtrl('slot_capacity')
 
   const { setting: guidedVisitPriceSetting, setSetting: setGuidedVisitPriceSetting, saveSetting: saveGuidedVisitPriceSetting, description: guidedVisitPriceDescription, setDescription: setGuidedVisitPriceDescription } = settingNumCtrl('guided_tour_price')
+  const { setting: giftCodePriceSetting, setSetting: setGiftCodePriceSetting, saveSetting: saveGiftCodePriceSetting, description: giftCodePriceDescription, setDescription: setGiftCodePriceDescription } = settingNumCtrl('gift_code_price')
 
   return (
     <Show when={can()}>
-      <div class="h-full w-full  relative overflow-y-auto ">
+      <div class="h-full w-full gap-4 flex flex-col relative overflow-y-auto ">
         <header class="flex justify-between items-center gap2">
           <h1 class="text-2xl font-bold">Réglages</h1>
-          <div class="flex items-center justify-end gap-2">
-            <SettingNumModal
-              title="Modifier la capacité"
-              value={setting()}
-              setValue={(value: number) => setSetting(value)}
-              onClose={(onClose: () => void) => saveSetting(onClose)}
-              description={description()}
-              setDescription={(description: string) => setDescription(description)}
-            />
-            <SettingNumModal
-              title="Modifier le créneau"
-              value={slotSetting()}
-              setValue={(value: number) => setSlotSetting(value)}
-              onClose={(onClose: () => void) => saveSlotSetting(onClose)}
-              description={slotDescription()}
-              setDescription={(description: string) => setSlotDescription(description)}
-            />
-            <SettingNumModal
-              title="Modifier le prix de la visite guidée"
-              isFloat={true}
-              value={guidedVisitPriceSetting()}
-              setValue={(value: number) => setGuidedVisitPriceSetting(value)}
-              onClose={(onClose: () => void) => saveGuidedVisitPriceSetting(onClose)}
-              description={guidedVisitPriceDescription()}
-              setDescription={(description: string) => setGuidedVisitPriceDescription(description)}
-            />          </div>
         </header>
-        <div class="italic text-gray-500 text-sm my-3">
-          {description()}
+
+        <div class="flex flex-col items-start p-2 border border-gray-200 rounded-lg">
+          <div class="flex items-center gap-2">
+            <span class="font-bold text-5xl text-amber-500">{setting() || 0} </span> visiteur{setting() > 1 ? 's' : ''}
+          </div>
+          <div class="italic text-gray-500 text-sm my-3">
+            {description()}
+          </div>
+          <SettingNumModal
+            title="Modifier la capacité"
+            value={setting()}
+            setValue={(value: number) => setSetting(value)}
+            onClose={(onClose: () => void) => saveSetting(onClose)}
+            description={description()}
+            setDescription={(description: string) => setDescription(description)}
+          />
         </div>
-        <div class="flex items-center gap-2">
-          <span class="font-bold text-5xl text-amber-500">{setting() || 0} </span> visiteur{setting() > 1 ? 's' : ''}
+        <div class="flex flex-col items-start p-2 border border-gray-200 rounded-lg">
+          <div class="flex items-center gap-2">
+            <span class="font-bold text-5xl text-amber-500">{slotSetting() || 0} </span> heure{slotSetting() > 1 ? 's' : ''}
+          </div>
+          <div class="italic text-gray-500 text-sm my-3">
+            {slotDescription()}
+          </div>
+          <SettingNumModal
+            title="Modifier le créneau"
+            value={slotSetting()}
+            setValue={(value: number) => setSlotSetting(value)}
+            onClose={(onClose: () => void) => saveSlotSetting(onClose)}
+            description={slotDescription()}
+            setDescription={(description: string) => setSlotDescription(description)}
+          />
         </div>
-        <div class="italic text-gray-500 text-sm my-3">
-          {slotDescription()}
+        <div class="flex flex-col items-start p-2 border border-gray-200 rounded-lg">
+          <div class="flex items-center gap-2">
+            <span class="font-bold text-5xl text-amber-500">{guidedVisitPriceSetting() || 0} </span> €</div>
+          <div class="italic text-gray-500 text-sm my-3">
+            {guidedVisitPriceDescription()}
+          </div>
+          <SettingNumModal
+            title="Modifier le prix de la visite guidée"
+            isFloat={true}
+            value={guidedVisitPriceSetting()}
+            setValue={(value: number) => setGuidedVisitPriceSetting(value)}
+            onClose={(onClose: () => void) => saveGuidedVisitPriceSetting(onClose)}
+            description={guidedVisitPriceDescription()}
+            setDescription={(description: string) => setGuidedVisitPriceDescription(description)}
+          />
         </div>
-        <div class="flex items-center gap-2">
-          <span class="font-bold text-5xl text-amber-500">{slotSetting() || 0} </span> heure{slotSetting() > 1 ? 's' : ''}
-        </div>
-        <div class="italic text-gray-500 text-sm my-3">
-          {guidedVisitPriceDescription()}
-        </div>
-        <div class="flex items-center gap-2">
-          <span class="font-bold text-5xl text-amber-500">{guidedVisitPriceSetting() || 0} </span> €
+        <div class="flex flex-col items-start p-2 border border-gray-200 rounded-lg">
+          <div class="flex items-center gap-2">
+            <span class="font-bold text-5xl text-amber-500">{giftCodePriceSetting() || 0} </span> €
+          </div>
+          <div class="italic text-gray-500 text-sm my-3">
+            {giftCodePriceDescription()}
+          </div>
+          <SettingNumModal
+            title="Modifier le prix du code cadeau"
+            value={giftCodePriceSetting()}
+            setValue={(value: number) => setGiftCodePriceSetting(value)}
+            onClose={(onClose: () => void) => saveGiftCodePriceSetting(onClose)}
+            description={giftCodePriceDescription()}
+            setDescription={(description: string) => setGiftCodePriceDescription(description)}
+          />
         </div>
       </div >
     </Show>
