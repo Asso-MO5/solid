6135f57342af1d28bbb108da637dc9a5abb4832e
Kazerlelutin <b@bouteiller.contact>
2026-01-13 21:20:22 +0100

    modification des websockets

--- src/features/msg/ms.ts
+++ src/features/msg/ms.ts
@@ -15,9 +15,16 @@ export const WebSocketProvider = ({
     if (typeof window === 'undefined') {
       return;
     }
-    ws = createWS(clientEnv.VITE_OCELOT_WS);
+    ws = createWS(clientEnv.VITE_WS);
+    ws.addEventListener("open", () => {
+      if (ws) {
+        ws.send(JSON.stringify({
+          action: "subscribe",
+          provider: "ocelot",
+        }));
+      }
+    });
     ws.addEventListener("message", (ev) => {
-
       const data = ev.data.startsWith('{') ? JSON.parse(ev.data) : ev.data;
       if (data.action) {
         handlers?.[data.room]?.(data.room, data.action);
