39959e5789d61dc249cc0719b823a3182e7e13af
Kazerlelutin <79605267+kazerlelutin@users.noreply.github.com>
2026-01-21 18:03:29 +0100

    prevent double fetch (#27)

--- src/features/stats/stats.ws-handler.ts
+++ src/features/stats/stats.ws-handler.ts
@@ -1,5 +1,6 @@
 import { clientEnv } from "~/env/client"
 import { setStats } from "./stats.store";
+import { createSignal } from "solid-js";
 
 const url = {
   current_visitors: `/museum/capacity/current`,
@@ -8,30 +9,39 @@ const url = {
   visitors_stats: "/museum/tickets/weekly-slots-stats"
 }
 
+const [isLoading, setIsLoading] = createSignal(false);
 
 export const statsWsHandler = async (room: keyof typeof url) => {
+  if (isLoading()) return;
+  setIsLoading(true);
 
+  try {
 
-  const response = await fetch(`${clientEnv.VITE_OCELOT_URL}${url[room]}`, {
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json'
+    const response = await fetch(`${clientEnv.VITE_OCELOT_URL}${url[room]}`, {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch stats');
     }
-  });
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch stats');
-  }
+    const data = await response.json();
+    if (room === 'tickets_stats') {
+      statsWsHandler('bank_stats');
+      statsWsHandler('visitors_stats');
+    }
 
-  const data = await response.json();
-  if (room === 'tickets_stats') {
-    statsWsHandler('bank_stats');
-    statsWsHandler('visitors_stats');
-  }
-
-  if (room === 'bank_stats' || room === 'visitors_stats') {
-    setStats(room, data);
-  } else {
-    setStats(room, data[room]);
+    if (room === 'bank_stats' || room === 'visitors_stats') {
+      setStats(room, data);
+    } else {
+      setStats(room, data[room]);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setIsLoading(false);
   }
 }
