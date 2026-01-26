1d003c84ac113d403f2521c6a10faa526fd7aa3b
Kazerlelutin <b@bouteiller.contact>
2026-01-26 11:51:27 +0100

    fix modal

--- src/features/setting/setting-num.modal.tsx
+++ src/features/setting/setting-num.modal.tsx
@@ -12,30 +12,21 @@ type SettingNumModalProps = {
 }
 
 export const SettingNumModal = (props: SettingNumModalProps) => {
-  const [isOpen, setIsOpen] = createSignal(false)
   const modal = ModalCtrl()
 
-  createEffect(() => {
-    if (isOpen()) {
-      modal.open({
-        title: props.title,
-        content: (
-          <div class="flex flex-col gap-4">
-            <input type="number" value={props.value} step={props.isFloat ? '0.10' : '1'} onChange={(e) => props.setValue(Number(e.target.value))} />
-            <textarea value={props.description || ''} onChange={(e) => props.setDescription(e.target.value)} />
-            <div class="flex items-center gap-2 justify-end">
-              <button onClick={() => setIsOpen(false)} class="secondary">Annuler</button>
-              <button onClick={() => props.onClose(() => setIsOpen(false))}>Enregistrer</button>
-            </div>
-          </div>
-        ),
-        size: 'lg',
-        closable: true,
-      })
-    } else {
-      modal.close()
-    }
-  })
-
-  return <button onClick={() => setIsOpen(true)}>{props.title}</button>
+  return <button onClick={() => modal.open({
+    title: props.title,
+    content: (
+      <div class="flex flex-col gap-4">
+        <input type="number" value={props.value} step={props.isFloat ? '0.10' : '1'} onChange={(e) => props.setValue(Number(e.target.value))} />
+        <textarea value={props.description || ''} onChange={(e) => props.setDescription(e.target.value)} />
+        <div class="flex items-center gap-2 justify-end">
+          <button onClick={() => modal.close()} class="secondary">Annuler</button>
+          <button onClick={() => props.onClose(() => modal.close())}>Enregistrer</button>
+        </div>
+      </div>
+    ),
+    size: 'lg',
+    closable: true,
+  })}>{props.title}</button>
 }
